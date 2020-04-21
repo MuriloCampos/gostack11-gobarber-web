1861953ce2db3167790837f7ff7d08766a7bae84
Murilo Campos <murilo13campos@gmail.com>
2020-04-21 10:02:55 -0400

    creating and styling SignIn and SignUp pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
-const App: React.FC = () => <h1>Hello World</h1>;
+import GlobalStyle from './styles/global';
+// import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+
+const App: React.FC = () => (
+  <>
+    <SignUp />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+
+import logoImg from '../../assets/logo.svg';
+
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
+import { Container, Content, Background } from './styles';
+
+const SignUp: React.FC = () => (
+  <Container>
+    <Background />
+    <Content>
+      <img src={logoImg} alt="GoBarber" />
+
+      <form>
+        <h1>Faca seu cadastro</h1>
+
+        <Input icon={FiUser} name="name" placeholder="Nome" />
+        <Input icon={FiMail} name="email" placeholder="E-mail" />
+        <Input
+          icon={FiLock}
+          name="password"
+          type="password"
+          placeholder="Senha"
+        />
+
+        <Button type="submit">Cadastrar</Button>
+      </form>
+
+      <a href="login">
+        <FiArrowLeft />
+        Voltar para logon
+      </a>
+    </Content>
+  </Container>
+);
+
+export default SignUp;
